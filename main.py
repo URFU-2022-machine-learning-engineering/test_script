4fbf18847196357c77c4916526a941bb0888f989
Vladimir Katin <katin.v.v@gmail.com>
2024-03-14 00:10:03 +0300

    add season emulation

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from tqdm import tqdm
 import glob
 import random
 import time
+from datetime import datetime
 
 
 def send_file(endpoint, file_name, file_content):
@@ -28,6 +29,13 @@ def find_audio_files(directory: str):
                 yield audio_file
 
 
+def send_random_file(audio_files, endpoint):
+    if audio_files:
+        random_file = random.choice(audio_files)
+        file_name = os.path.basename(random_file)
+        send_file(endpoint, file_name, random_file)
+
+
 def send_audio_files(directory: str, endpoint: str):
     audio_files = list(find_audio_files(directory))
     if not audio_files:
@@ -37,7 +45,22 @@ def send_audio_files(directory: str, endpoint: str):
     for audio_file in tqdm(audio_files, desc="Sending audio files"):
         file_name = os.path.basename(audio_file)
         send_file(endpoint, file_name, audio_file)
-        time.sleep(random.randint(1, 13))  # Simulate network or processing delays
+
+        current_hour = datetime.now().hour
+        if 8 <= current_hour < 12:  # Morning
+            print("Morning time")
+            time.sleep(random.randint(1, 5))  # More frequent requests in the morning
+            send_random_file(audio_files, endpoint)  # Send an additional random file
+        elif 12 <= current_hour < 18:  # Day time (excluding morning and evening)
+            print("Day time")
+            time.sleep(random.randint(6, 10))
+        elif 18 <= current_hour < 20:  # Evening
+            print("Evening time")
+            time.sleep(random.randint(2, 7))  # More frequent requests in the evening
+            send_random_file(audio_files, endpoint)  # Send an additional random file
+        else:  # Night time
+            print("Night time")
+            time.sleep(random.randint(11, 15))  # Less frequent requests at night
 
 
 if __name__ == "__main__":
